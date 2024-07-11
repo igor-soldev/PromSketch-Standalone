09967ff73372682eee4037c178e7cc8f3a3ac805
Kenny Wu <43484854+KennyWu@users.noreply.github.com>
2024-07-10 22:17:28 -0700

    change scripts

--- ExporterStarter/run_evaluation_10000samples.sh
+++ ExporterStarter/run_evaluation_10000samples.sh
@@ -3,18 +3,21 @@ python ExportManager.py --config=num_samples_config.yml --targets=1 --timeseries
 python ExportManager.py --config=num_samples_config.yml --targets=1 --timeseries=100 --windowsize=10000 --querytype=sum --waiteval=50
 python ExportManager.py --config=num_samples_config.yml --targets=2 --timeseries=1000 --windowsize=10000 --querytype=sum --waiteval=50
 python ExportManager.py --config=num_samples_config.yml --targets=20 --timeseries=10000 --windowsize=10000 --querytype=sum --waiteval=100
-python ExportManager.py --config=num_samples_config.yml --targets=200 --timeseries=55000 --windowsize=10000 --querytype=sum --waiteval=100
+python ExportManager.py --config=num_samples_config.yml --targets=30 --timeseries=15000 --windowsize=10000 --querytype=sum --waiteval=100
+python ExportManager.py --config=num_samples_config.yml --targets=100 --timeseries=50000 --windowsize=10000 --querytype=sum --waiteval=100
 python ExportManager.py --config=num_samples_config.yml --targets=1 --timeseries=1 --windowsize=10000 --querytype=avg --waiteval=50
 python ExportManager.py --config=num_samples_config.yml --targets=1 --timeseries=10 --windowsize=10000 --querytype=avg --waiteval=50
 python ExportManager.py --config=num_samples_config.yml --targets=1 --timeseries=100 --windowsize=10000 --querytype=avg --waiteval=50
 python ExportManager.py --config=num_samples_config.yml --targets=2 --timeseries=1000 --windowsize=10000 --querytype=avg --waiteval=50
 python ExportManager.py --config=num_samples_config.yml --targets=20 --timeseries=10000 --windowsize=10000 --querytype=avg --waiteval=100
-python ExportManager.py --config=num_samples_config.yml --targets=200 --timeseries=55000 --windowsize=10000 --querytype=avg --waiteval=100
+python ExportManager.py --config=num_samples_config.yml --targets=30 --timeseries=15000 --windowsize=10000 --querytype=avg --waiteval=100
+python ExportManager.py --config=num_samples_config.yml --targets=100 --timeseries=50000 --windowsize=10000 --querytype=avg --waiteval=100
 python ExportManager.py --config=num_samples_config.yml --targets=1 --timeseries=1 --windowsize=10000 --querytype=quantile --waiteval=50
 python ExportManager.py --config=num_samples_config.yml --targets=1 --timeseries=10 --windowsize=10000 --querytype=quantile --waiteval=100
 python ExportManager.py --config=num_samples_config.yml --targets=1 --timeseries=100 --windowsize=10000 --querytype=quantile --waiteval=100
 python ExportManager.py --config=num_samples_config.yml --targets=2 --timeseries=1000 --windowsize=10000 --querytype=quantile --waiteval=200
 python ExportManager.py --config=num_samples_config.yml --targets=20 --timeseries=10000 --windowsize=10000 --querytype=quantile --waiteval=300
-python ExportManager.py --config=num_samples_config.yml --targets=200 --timeseries=55000 --windowsize=10000 --querytype=quantile --waiteval=300
+python ExportManager.py --config=num_samples_config.yml --targets=30 --timeseries=15000 --windowsize=10000 --querytype=quantile --waiteval=300
+python ExportManager.py --config=num_samples_config.yml --targets=100 --timeseries=50000 --windowsize=10000 --querytype=quantile --waiteval=300
 
 /bin/bash ./kill.sh
--- ExporterStarter/run_evaluation_10000samples_debug.sh
+++ ExporterStarter/run_evaluation_10000samples_debug.sh
@@ -1,3 +1,5 @@
 python ExportManager.py --config=num_samples_config.yml --targets=200 --timeseries=100000 --windowsize=10000 --querytype=sum --waiteval=150
 python ExportManager.py --config=num_samples_config.yml --targets=200 --timeseries=100000 --windowsize=10000 --querytype=avg --waiteval=150
 python ExportManager.py --config=num_samples_config.yml --targets=200 --timeseries=100000 --windowsize=10000 --querytype=quantile --waiteval=300
+
+/bin/bash ./kill.sh

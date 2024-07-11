fe498b69b42f0a97c4801b4d444681938d1346c4
Kenny Wu <43484854+KennyWu@users.noreply.github.com>
2024-07-11 16:02:18 -0700

    change scripts to support 100000 series

--- ExporterStarter/run_evaluation_test.sh
+++ ExporterStarter/run_evaluation_test.sh
@@ -1,3 +1,3 @@
-python ExportManager.py --config=num_samples_config.yml --targets=200 --timeseries=55000 --windowsize=100 --querytype=avg --waiteval=40
+python ExportManager.py --config=num_samples_config.yml --targets=200 --timeseries=100000 --windowsize=100 --querytype=avg --waiteval=40
 
 /bin/bash ./kill.sh

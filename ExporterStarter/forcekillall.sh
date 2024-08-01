0c01954c016f6cabeda712f2af47abc0bfac86bc
Kenny Wu <43484854+KennyWu@users.noreply.github.com>
2024-07-31 21:14:44 -0700

    add force kill

---
+++ ExporterStarter/forcekillall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+kill -9  $(ps aux | grep './vmalert' | awk '{print $2}')
+kill -9 $(ps aux | grep './victoria-metrics' | awk '{print $2}')
+kill -9 $(ps aux | grep  -i '[p]ython fake_norm_exporter.py' | awk '{print $2}')
+kill -9 $(ps aux | grep  -i '[p]ython ExportManager.py' | awk '{print $2}')
+kill -9 $(ps aux | grep  -i 'sh run_evaluation_10000ts_avg.sh' | awk '{print $2}')
+kill -9 $(ps aux | grep  -i 'sh run_evaluation_10000ts_quantile.sh' | awk '{print $2}')
+kill -9 $(ps aux | grep -i '[p]ython ../EvaluationTools/EvalData.py' | awk '{print $2}')
+
+rm -r victoria-metrics-data/
